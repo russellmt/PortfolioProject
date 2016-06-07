0d161ca76de543550f2d6d22c9b8d200c3e6b220
Dillon Cordova <dillon_cordova@mcgraw-hill.com>
2016-06-07 16:06:17 -0700

    asd

--- app/js/modules/dillonPage/controllers/resumeController.js
+++ app/js/modules/dillonPage/controllers/resumeController.js
@@ -5,32 +5,45 @@
 
     .controller('resumeController', [function () {
         var allTextInCurFile = '';
-
-        this.readFile = function (file) {
-            debugger;
+        this.allTextLines = [];
+        var me = this;
+        
+        this.readFile = function (fileName) {
             var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", file, false);
-
+            rawFile.open("GET", fileName, false);
             rawFile.onreadystatechange = function () {
                 if(rawFile.readyState === 4) {
                     if(rawFile.status === 200 || rawFile.status == 0) {
                         allTextInCurFile = rawFile.responseText;
+                        me.allTextLines = allTextInCurFile.split('\n');
                     }
                 }
             };
             rawFile.send(null);
         };
         
-        this.readFile('../../../resumes/dillonResume.txt');
+        this.decodeFile = function() {
+            debugger;
+            for(var lineElement in this.allTextLines) {
+                var curLine = this.allTextLines[lineElement];
+                
+                for(var charElement in curLine) {
+                    var char = curLine[charElement];
+                    switch (char) {
+                        case 'asd':
+                            break;
 
-        this.decodeFile = function(word) {
-            switch (word) {
-                case 'asd':
-                    break;
-
-                case 'aaa':
-                    break;
+                        case 'aaa':
+                            break;
+                    }
+                }
+                
             }
+
         };
+
+        // this.readFile('../../../resumes/dillonResume.txt');
+        // this.decodeFile();
+        // debugger;
     }]);
 })();
