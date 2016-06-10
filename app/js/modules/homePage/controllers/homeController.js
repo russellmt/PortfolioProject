596089e69653cd2a71e0f2cd30512fe76013fdf5
russell_templet <russell.templet@jdpa.com>
2016-06-10 11:34:58 -0700

    loading done, home page progress

--- app/js/modules/homePage/controllers/homeController.js
+++ app/js/modules/homePage/controllers/homeController.js
@@ -1,9 +1,53 @@
 (function(){
     'use strict';
 
+    var ORDER = [
+        'ball',
+        'fan',
+        'mushroom',
+        'clamp',
+        'scythe',
+        'cat',
+        'hat',
+        'note',
+        'anemone',
+        'cross',
+        'dots',
+        'rectangle'
+    ];
+
     angular.module('app.homePage')
 
-    .controller('HomeController', [function () {
-        
+    .controller('HomeController', ['$timeout', function ($timeout) {
+        function animateLoading() {
+            var timeline = new TimelineLite();
+            var path = document.getElementById(ORDER[0]);
+            var svg = document.getElementById('loadingSvg');
+            var loadBg = document.getElementById('loadBg');
+
+            for (var i = 1, len = ORDER.length; i < len; i++) {
+                timeline.to(path, 0.25, {
+                    morphSVG: '#' + ORDER[i]
+                });
+            }
+            timeline.to(svg, 2, {scale: 8}, '-=.25')
+                .to(path, 1, {fill: 'white'}, '-=2')
+                .to(loadBg, 2, {opacity: 0}, '-=1.3')
+                .set(path, {visibility: 'hidden'})
+                .set(svg, {visibility: 'hidden'})
+                .set(loadBg, {visibility: 'hidden'});
+        }
+
+        $timeout(function() {
+            animateLoading();
+            particleground(document.getElementById('bg'), {
+                dotColor: "#333333",
+                lineColor: "#B8E986",
+                density: 13000
+
+            });
+        });
+
+
     }]);
 })();
