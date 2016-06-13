b9ab901f53fea334e478ca5b95281064aaf7c8b8
russell_templet <russell.templet@jdpa.com>
2016-06-13 15:43:43 -0700

    crude bubble and emerald animations (prob need to generalize code)

--- app/js/modules/homePage/controllers/homeController.js
+++ app/js/modules/homePage/controllers/homeController.js
@@ -1,53 +1,29 @@
 (function(){
     'use strict';
 
-    var ORDER = [
-        'ball',
-        'fan',
-        'mushroom',
-        'clamp',
-        'scythe',
-        'cat',
-        'hat',
-        'note',
-        'anemone',
-        'cross',
-        'dots',
-        'rectangle'
-    ];
-
     angular.module('app.homePage')
 
-    .controller('HomeController', ['$timeout', function ($timeout) {
-        function animateLoading() {
-            var timeline = new TimelineLite();
-            var path = document.getElementById(ORDER[0]);
-            var svg = document.getElementById('loadingSvg');
-            var loadBg = document.getElementById('loadBg');
+    .controller('HomeController', ['$timeout', 'bubbleService', 'gemService', function ($timeout, bubbleService, gemService) {
+        var animationServiceMap = {
+            lavi: gemService,
+            russell: bubbleService
+        };
 
-            for (var i = 1, len = ORDER.length; i < len; i++) {
-                timeline.to(path, 0.25, {
-                    morphSVG: '#' + ORDER[i]
-                });
-            }
-            timeline.to(svg, 2, {scale: 8}, '-=.25')
-                .to(path, 1, {fill: 'white'}, '-=2')
-                .to(loadBg, 2, {opacity: 0}, '-=1.3')
-                .set(path, {visibility: 'hidden'})
-                .set(svg, {visibility: 'hidden'})
-                .set(loadBg, {visibility: 'hidden'});
-        }
-
+        var scope = this;
         $timeout(function() {
-            animateLoading();
             particleground(document.getElementById('bg'), {
                 dotColor: "#333333",
                 lineColor: "#B8E986",
                 density: 13000
-
             });
         });
 
+        this.handleEmployeeEnter = function(key) {
+            animationServiceMap[key].generateAnimation();
+        };
 
+        this.handleEmployeeLeave = function(key) {
+            animationServiceMap[key].resetAnimation();
+        };
     }]);
 })();
